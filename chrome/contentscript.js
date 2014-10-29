2b2319cccb912cc430735cffd541ac86ec14ea5e
mdesenfants <matt.desenfants@gmail.com>
2014-10-28 22:22:55 -0500

    updating chrome extension to address bug with reddit enhancement suite

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,5 +1,12 @@
-$("a.author").each(function() {
+createLinks = function() {
   author = $(this);
   margin = author.css("margin-right");
-  $(this).after('<a target="_blank" style="margin-right: '+margin+'" href="http://imgwaffle.com/'+author.html()+'">(imgwaffle)</a>');
+  $(this).after('<a class="waffle" target="_blank" style="margin-right: '+margin+'" href="http://imgwaffle.com/'+author.html()+'">(imgwaffle)</a>');
+}
+
+$("a.author").each(createLinks);
+
+$(window).on('hashchange', function() {
+  $(".waffle").remove();
+  $("a.author").each(createLinks)
 });
